9ba2ae51519c19e8c4fc7e9eb77dc2dbb17df057
dev-edx <d.bhuptani@edexa.team>
2024-10-14 18:43:41 +0530

    feat: updated database to mysql

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -1,12 +1,16 @@
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-
-dotenv.config();
+const mysql = require('mysql2/promise');
+import config from '.';
 
 const connectToDataBase = async () => {
   try {
-    mongoose.connect(process.env.DB);
+    const connection = await mysql.createConnection({
+      host: config.DB_HOST,
+      user: config.DB_USER,
+      password: config.DB_PASSWORD,
+      database: config.DB_NAME,
+    });
     console.log('Connected to the database');
+    return connection;
   } catch (error) {
     console.log('catch', error);
   }
--- backend/config/index.ts
+++ backend/config/index.ts
@@ -3,7 +3,11 @@ dotenv.config();
 
 export default {
   PORT: process.env.PORT,
-  DATABASE: process.env.DB,
+  DB_PORT: process.env.DB_PORT,
+  DB_HOST: process.env.DB_HOST,
+  DB_USER: process.env.DB_USER,
+  DB_NAME: process.env.DB_NAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,
   SALT_ROUND: parseInt(`${process.env.SALT_ROUND}`),
   SECRET_KEY: process.env.SECRET_KEY,
   JWT_EXPIRE: process.env.JWT_EXPIRE
